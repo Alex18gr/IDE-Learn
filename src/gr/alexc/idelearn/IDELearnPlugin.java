20de2ea2064451781355a441cfabce34fbffc7d3
Alexandros Tsichouridis <alexchih02@gmail.com>
2020-11-19 00:43:08 +0200

    Add JDT support and project properties

--- src/gr/alexc/idelearn/IDELearnPlugin.java
+++ src/gr/alexc/idelearn/IDELearnPlugin.java
@@ -3,16 +3,17 @@ package gr.alexc.idelearn;
 import javax.inject.Inject;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ProjectScope;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.preferences.IScopeContext;
 import org.eclipse.equinox.log.Logger;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.BundleListener;
+import org.osgi.service.prefs.BackingStoreException;
+import org.osgi.service.prefs.Preferences;
 
 public class IDELearnPlugin extends AbstractUIPlugin {
 	
@@ -49,7 +50,20 @@ public class IDELearnPlugin extends AbstractUIPlugin {
 				IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
 				
 				for (IProject project : projects) {
-					logger.debug(project.getName());
+					// logger.debug(project.getName());
+					
+					
+					IScopeContext projectScope = new ProjectScope(project);
+					Preferences projectNode = projectScope.getNode(PLUGIN_ID);
+					if (projectNode != null) {
+						projectNode.put("exersiceId", "001");
+						try {
+							projectNode.flush();
+						} catch (BackingStoreException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
 				}
 				
 			}
